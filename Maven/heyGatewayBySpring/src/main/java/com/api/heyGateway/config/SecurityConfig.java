844cf0531375e434eab7eccef6a01198542f6785
Houssam BACAR <houssambacar67@gmail.com>
2026-03-06 07:43:39 +0100

    Ajout des features : - Attention !!! /api/movies doit etre modifié et supprimer de   securityConfig - à travailler pour permettre l'authentification 	modified:   src/main/java/com/api/heyGateway/config/SecurityConfig.java 	modified:   src/main/resources/application.yml

--- Maven/heyGatewayBySpring/src/main/java/com/api/heyGateway/config/SecurityConfig.java
+++ Maven/heyGatewayBySpring/src/main/java/com/api/heyGateway/config/SecurityConfig.java
@@ -4,6 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
 
 @Configuration
 public class SecurityConfig {
@@ -17,6 +25,7 @@ public class SecurityConfig {
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/api/authentication/**").permitAll()
                         .pathMatchers("/test").permitAll()
+                        .pathMatchers("/api/movies/**").permitAll()
                         .anyExchange().authenticated()
                 )
 
@@ -25,5 +34,42 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    // CORS pour autoriser le frontend Vue .. en Vue.js
+    @Bean
+    public CorsWebFilter corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("http://localhost:12105"); // Frontend
+        config.setAllowCredentials(true);
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.addAllowedOrigin("http://localhost:12105");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
     }
 }
